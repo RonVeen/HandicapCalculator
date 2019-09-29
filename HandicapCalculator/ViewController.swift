d120db8370598ac32976e7ae81b7171fca76e058
Ron Veen <ronveen@gmail.com>
2019-09-29 21:50:38 +0200

    Initial Commit

---
+++ HandicapCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HandicapCalculator
+//
+//  Created by R Veen on 29/09/2019.
+//  Copyright © 2019 R Veen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
